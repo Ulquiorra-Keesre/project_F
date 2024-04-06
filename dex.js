b918cb7d062fd8d447f02cfca8fd80af8773ebcf
Ulquiorra-Keesre <sinukmaksim@gmail.com>
2024-04-06 23:10:18 +0300

    western style

---
+++ dex.js
@@ -0,0 +1,28 @@
+function getTimeInTimeZone(timezone) {
+    const now = new Date();
+    const options = {
+        timeZone: timezone,
+        hour12: true,
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+    return now.toLocaleTimeString('ru-RU', options);
+}
+function updateClocks() {
+    document.getElementById('NYC').textContent = `Нью-Йорк: ${getTimeInTimeZone('America/New_York')}`;
+    document.getElementById('cal').textContent = `Калифорния: ${getTimeInTimeZone('America/Los_Angeles')}`;
+    document.getElementById('tok').textContent = `Токио: ${getTimeInTimeZone('Asia/Tokyo')}`;
+}
+
+setInterval(updateClocks, 1000);
+updateClocks();
+
+document.getElementById('toggleButton_1').addEventListener('click', function() {
+    document.body.classList.toggle('medieval-style');
+    document.querySelectorAll('.container').forEach(function(container) {
+        container.classList.toggle('medieval-style'); 
+    });
+    document.querySelector("header").classList.toggle('medieval-style');
+    document.getElementById("toggleButton_1").classList.toggle('medieval-style');
+});
