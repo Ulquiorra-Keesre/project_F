f137c7e231ad1da1b533e5f1828a555f9ddd3f15
Ulquiorra-Keesre <sinukmaksim@gmail.com>
2024-04-07 09:12:51 +0300

    24h

--- dex.js
+++ dex.js
@@ -2,7 +2,7 @@ function getTimeInTimeZone(timezone) {
     const now = new Date();
     const options = {
         timeZone: timezone,
-        hour12: true,
+        hour12: false,
         hour: 'numeric',
         minute: 'numeric',
         second: 'numeric'
